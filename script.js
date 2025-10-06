8904d1dff7c5831aafb2462f7fcce3347f4b3067
jdd243 <151496031+jdd243@users.noreply.github.com>
2025-10-06 13:45:28 -0700

    refactor play game to add a reset button,
    reset game funtion sets all the scores and counters to 0.
    
    moved all the function element consts to the top of the function

--- script.js
+++ script.js
@@ -35,8 +35,25 @@ function playGame() {
     let roundsPlayed = 0;
     const roundsToPlay = 5;
 
-    const buttons = document.querySelectorAll('.player-selection');
+    const playerScoreEl = document.getElementById("player-score");
+    const computerScoreEl = document.getElementById("computer-score");
+    const roundsPlayedEl = document.getElementById("rounds-played");
+    const winnerMessageEl = document.getElementById("winner-message");
+    const controlEl = document.getElementById("controls");
 
+    function resetGame() {
+    userScore = 0;
+    computerScore = 0;
+    roundsPlayed = 0;
+
+    playerScoreEl.textContent = 0;
+    computerScoreEl.textContent = 0;
+    roundsPlayedEl.textContent = 0;
+    winnerMessageEl.textContent = "";
+    controlEl.innerHTML = "";
+    }
+
+    const buttons = document.querySelectorAll('.player-selection');
     buttons.forEach(button => {
         button.addEventListener("click", (element) => {
             if (roundsPlayed >= roundsToPlay) return;
@@ -47,30 +64,26 @@ function playGame() {
 
             if (roundResults === "you win!") userScore++;
             else if (roundResults === "Computer wins!") computerScore++;
-
             roundsPlayed++;
-
-            const playerScoreEl = document.getElementById("player-score");
+            
             playerScoreEl.textContent = userScore;
-
-            const computerScoreEl = document.getElementById("computer-score");
             computerScoreEl.textContent = computerScore;
-
-            const roundsPlayedEl = document.getElementById("rounds-played");
             roundsPlayedEl.textContent = roundsPlayed;
 
-            if(roundsPlayed === roundsToPlay) {
-                
-                const winnerMessageEl = document.getElementById("winner-message");
+            if(roundsPlayed === roundsToPlay) {            
 
                 if (userScore > computerScore) winnerMessageEl.textContent = "You win!";
                 else if (userScore < computerScore) winnerMessageEl.textContent = "Computer Wins!";
                 else winnerMessageEl.textContent = "Tie game!";
-            }
-
-            
-        })
-    })
+               
+                const restartBtn = document.createElement("button");
+                restartBtn.id = "new-game";
+                restartBtn.textContent = "Play again!";
+                restartBtn.addEventListener("click", resetGame);
+                controlEl.appendChild(restartBtn);
+            }                       
+        });
+    });
 
 
 }
